987e0121acdfab4cc3076369b6c2843485ce1540
iamqjin <iamqjin@gmail.com>
2017-01-13 18:01:04 +0700

    녹음 타이머 기능 추가
    녹음 타이머기능 추가

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -13,18 +13,24 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     
     var audioRecorder: AVAudioRecorder!
     
+    var timer = Timer()
+    
     var soundFileURL:URL!
     
+    //재녹음 이용
     var recordingResume = false
     var recordingFirst = false
+    
     
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var statusLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         stopRecordingButton.isEnabled = false
+        statusLabel.text = String(format: "%02d:%02d")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,11 +39,28 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     }
     
     
+
+    //타이머
+    func updateAudioMeter(_ timer:Timer) {
+        
+        if audioRecorder.isRecording {
+            let min = Int(audioRecorder.currentTime / 60)
+            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
+            let s = String(format: "%02d:%02d", min, sec)
+            statusLabel.text = s
+            audioRecorder.updateMeters()
+            // if you want to draw some graphics...
+            //var apc0 = recorder.averagePowerForChannel(0)
+            //var peak0 = recorder.peakPowerForChannel(0)
+        }
+    }
+    
     @IBAction func recordAudio(_ sender: AnyObject) {
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(RecordSoundsViewController.updateAudioMeter), userInfo: nil, repeats: true)
         
         recordingLabel.text = "Recording in progress"
         stopRecordingButton.isEnabled = true
-//        recordButton.isEnabled = false
         
         let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
         let recordingName = "recordedVoice.wav"
@@ -80,6 +103,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
+        timer.invalidate()
     }
     
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
